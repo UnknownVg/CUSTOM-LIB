0990112ee3174bfc5ce532841552585c457f2d27
UnknownVg <ruigie.gabs2@gmail.com>
2025-11-12 21:19:15 +0800

    Update ThemeManager.lua

--- Add-ons/ThemeManager.lua
+++ Add-ons/ThemeManager.lua
@@ -397,7 +397,7 @@ do
         groupbox:AddLabel("Font color"):AddColorPicker("FontColor", { Default = self.Library.Scheme.FontColor })
         groupbox:AddDropdown("FontFace", {
             Text = "Font Face",
-            Default = "Jura",
+            Default = "RobotoMono",
             Values = { "BuilderSans", "Code", "Fantasy", "Gotham", "Jura", "Roboto", "RobotoMono", "SourceSans" },
         })
 
